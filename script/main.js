b7db72f4f8783b5e59f47a7556fb421fbebc6c5a
Jarangos <jairarango10@gmail.com>
2022-12-01 01:24:51 -0500

    Se realizan modificaciones a la herramienta, añadiendo dos botones y mejorando la interfaz.

--- script/main.js
+++ script/main.js
@@ -45,3 +45,24 @@ function divivir() {
     document.getElementById("Respuesta").innerHTML = `El resultado de dividir ${num1} entre ${num2} es:`;
     document.getElementById("result").value=resultado;
 }
+function modular() {
+  let num1;
+  let num2;
+  let resultado;
+  num1 = document.getElementById("num1").value;
+  num2 = document.getElementById("num2").value;
+  resultado = parseInt(num1) % parseInt(num2);
+  document.getElementById("Respuesta").innerHTML = `El resultado de modular ${num1} entre ${num2} es:`;
+  document.getElementById("result").value=resultado;
+}
+
+function potenciar() {
+  let num1;
+  let num2;
+  let resultado;
+  num1 = document.getElementById("num1").value;
+  num2 = document.getElementById("num2").value;
+  resultado = Math.pow(parseInt(num1),parseInt(num2))
+  document.getElementById("Respuesta").innerHTML = `El resultado de potenciar ${num1} a ${num2} es:`;
+  document.getElementById("result").value=resultado;
+}
